7d9776004e5d60670e0826651b891af7cea531fe
garsar <saga0520@gmail.com>
2026-08-20 23:22:59 +0900

    問題データをフロントエンドの静的データとして管理 (#21)

---
+++ frontend/src/features/quiz/index.ts
@@ -0,0 +1,12 @@
+export { questions } from './data/question'
+
+export type {
+  PatternAQuestion,
+  PatternBQuestion,
+  PlayerType,
+  Question,
+  QuestionCondition,
+  QuestionPattern,
+  TileCode,
+  WinType,
+} from './types/question'
